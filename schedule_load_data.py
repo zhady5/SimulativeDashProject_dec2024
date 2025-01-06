895ff32c3608bef28f78eb6561679e6067ab823c
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-06 21:11:33 +0500

    Update schedule_load_data.py

--- schedule_load_data.py
+++ schedule_load_data.py
@@ -196,7 +196,7 @@ def job():
         print(f"Ошибка при выполнении задания: {e}")
 
 
-schedule.every().day.at("21:07").do(job)
+schedule.every().day.at("16:12").do(job)
 #schedule.every().day.at(utc_time(20, 39)).do(job)
 
 while True:
