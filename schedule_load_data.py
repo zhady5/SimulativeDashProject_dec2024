857e052d383b0ade62fb3ed21f767e4b51ee87ed
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-06 20:26:18 +0500

    Update schedule_load_data.py

--- schedule_load_data.py
+++ schedule_load_data.py
@@ -214,6 +214,7 @@ schedule.every().day.at(utc_time(9)).do(job)
 schedule.every().day.at(utc_time(13)).do(job)
 schedule.every().day.at(utc_time(17)).do(job)
 schedule.every().day.at(utc_time(21)).do(job)
+schedule.every().day.at(utc_time(18, 30)).do(job)
 
 while True:
     schedule.run_pending()
