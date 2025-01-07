40c143988b1ba8db5c031a1c0e6ef56e1e7388c1
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-07 21:51:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -270,7 +270,8 @@ def main():
         df_subset = df[columns_to_show]
         html_table = styled_df(df_subset).to_html()
         # Оборачиваем таблицу в div с фиксированной шириной и прокруткой
-        scrollable_table = f'<div style="overflow-x:auto;">{html_table}</div>'
+        #scrollable_table = f'<div style="overflow-x:auto;">{html_table}</div>'
+        scrollable_table = f'<div style="overflow-x: auto; overflow-y: auto; max-height: 300px;">{html_table}</div>'
         st.write(scrollable_table, unsafe_allow_html=True)
         #---------------------------------------------------------------------------------------------------------------------
         #Поисковик
