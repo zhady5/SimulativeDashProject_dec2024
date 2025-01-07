bd849a87e4e8834e29e379ab25fc4801f3ac4f63
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-07 21:45:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def main():
 
         #st.table(df[columns_to_show])
         df_subset = df[columns_to_show]
-        html_table = StickyHeader(styled_df(df_subset)).to_html()
+        html_table = styled_df(df_subset).to_html()
         # Оборачиваем таблицу в div с фиксированной шириной и прокруткой
         scrollable_table = f'<div style="overflow-x:auto;"> {html_table} </div> <style> .dataframe thead tr th { position: sticky; top: 0; background-color: white; z-index: 99; } </style>'
         #scrollable_table = f'<div style="overflow-x:auto;">{html_table}</div>'
