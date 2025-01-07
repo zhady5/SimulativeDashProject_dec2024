d82a2b72d5b258aac15be25a0880e3f49aca7655
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-07 21:22:39 +0500

    Update fig_table_views.py

--- fig_table_views.py
+++ fig_table_views.py
@@ -85,7 +85,7 @@ def styled_df(df):
       # Установка стиля для заголовков
     styled_df.set_table_styles([
         {'selector': 'th', 'props': [('color', '#8B0000')]},
-         {'selector': 'tr', 'props': [('border-bottom', '0.5px solid #deb887')]},
+         {'selector': 'tr', 'props': [('border-bottom', '0.5px solid gray')]},
     ])
     
 
