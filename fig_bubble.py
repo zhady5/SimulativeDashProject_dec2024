e1adf207e0e86beb123b3328907b1ff87f50817a
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-09 01:40:55 +0500

    Update fig_bubble.py

--- fig_bubble.py
+++ fig_bubble.py
@@ -29,7 +29,8 @@ def create_bubble_fig(filtered_df):
             mode='markers',
             marker=dict(
                 size=gr_pvr_sum['text_len'],
-                color=gr_pvr_sum['current_views'],
+                #color=gr_pvr_sum['current_views'],
+                color=gr_pvr_sum['text_len'],    
                 colorscale=colors,
                 showscale=False,  # Скрывает colorbar
                 sizemode='area',
