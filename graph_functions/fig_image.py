5049af548b677646fcc9706a67016b4dacfeb955
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-15 19:57:46 +0500

    Update fig_image.py

--- graph_functions/fig_image.py
+++ graph_functions/fig_image.py
@@ -6,7 +6,8 @@ import string
 import pandas as pd
 from collections import Counter
 import re
-from preparation_data.functions import  gradient_color_func #get_gradient_color,
+from PIL import ImageColor
+#from preparation_data.functions import  gradient_color_func #get_gradient_color,
 
 def load_stopwords_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -31,7 +32,17 @@ def clean_text(text):
     #stop_words = {'и', 'в', 'во', 'не', 'что', 'он', 'она', 'оно', 'они', 'но', 'а', 'это'}  # Простые стоп-слова
     words = [word for word in words if word not in dell_words]  # Удаляем стоп-слова
     return words
-    
+
+# Функция градиента для самих слов в облаке слов
+def gradient_color_func(word=None, font_size=None, position=None, orientation=None, font_path=None, random_state=None):
+    start_color = hex_to_rgb('#8B0000')
+    end_color = hex_to_rgb('#ffb347')
+    num_steps = 50  # Количество шагов равно количеству слов
+    colors = interpolate_color(start_color, end_color, num_steps)
+    index = random.randint(0, num_steps - 1)  # Случайное число от 0 до количества слов
+    r, g, b = colors[index]
+    return f"rgb({r}, {g}, {b})"
+
 
 @st.cache_data
 def prepare_data(posts, channel):
@@ -46,7 +57,11 @@ def plot_wordcloud(data):
     wc.fit_words(d)
     return wc.to_image()
 
-def make_image(df_words):
+def make_image(df_words, contr_color = '#f5dfbf'):
     img = BytesIO()
-    plot_wordcloud(data=df_words).save(img, format='PNG')
+    d = {a: x for a, x in df_words.values}
+    wc = WordCloud(background_color='#f5dfbf', color_func=gradient_color_func) 
+    wc.fit_words(d)
+    wc.to_image().save(img, format='PNG')
+    #plot_wordcloud(data=df_words).save(img, format='PNG')
     return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
